0e98c184027f896d32b7a479710b43930d95770f
Ahmed Hamdi <119865529+AhmedHamdi0@users.noreply.github.com>
2023-09-20 02:36:53 +0300

    Generalize number of channels in Communication_Module

--- Communication_Module.vhd
+++ Communication_Module.vhd
@@ -3,10 +3,13 @@ use ieee.std_logic_1164.all;
 -- use work.Common_Ports.all;
 
 entity Communication_Module is
-  port (
-    in_comm_channel: in std_logic_vector(7 downto 0);
-    out_comm_channel: out std_logic_vector(7 downto 0)
-  );
+	generic (
+		N: positive := 8 -- Number of input and output channels
+	);
+	port (
+		in_comm_channel: in std_logic_vector(N-1 downto 0);
+		out_comm_channel: out std_logic_vector(N-1 downto 0)
+	);
 end Communication_Module;
 
 architecture behav of Communication_Module is
