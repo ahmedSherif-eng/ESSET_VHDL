b98fd9c70655593359155f9f1da40ea3ed1f4feb
VEX <72231218+ahmedSherif-eng@users.noreply.github.com>
2023-11-29 15:03:54 +0200

    Add transmitter top level mapping

--- top_level.vhd
+++ top_level.vhd
@@ -11,7 +11,12 @@ entity Top_Level is
     in_toplvl_1bit_channel: in std_logic;
     out_toplvl_Nbit_channel: out std_logic_vector(N-1 downto 0);
     clk: in std_logic;
-    toplvl_status: out std_logic
+    toplvl_status: out std_logic;
+    in_TX_DV: in std_logic;
+    in_TX_Byte   : in  std_logic_vector(7 downto 0);
+    out_TX_Active : out std_logic;
+    out_TX_Serial : out std_logic;
+    out_TX_Done   : out std_logic
   );
 end Top_Level;
 
@@ -34,7 +39,17 @@ begin
 	 o_RX_DV => toplvl_status,
 	 o_RX_Byte => data_sniffing_out_buffer
 	 );
-  
+    --Instantiate UART TX
+    UART_Transmitter_Instance: entity work.UART_TX
+    port map(
+      i_Clk => clk,
+      i_TX_DV => in_TX_DV,
+      i_TX_Byte => in_TX_Byte,
+      o_TX_Active => out_TX_Active,
+      o_TX_Serial => out_TX_Serial,
+      o_TX_Done => out_TX_Done
+    );
+
   -- Instantiate Communication_Protocol
   Communication_Module_instance : entity work.Communication_Module
     port map (
@@ -42,6 +57,7 @@ begin
       in_comm_channel => data_sniffing_out_buffer,
       out_comm_channel => out_toplvl_Nbit_channel
     );
+
   
   -- Connect the status signal from Data_Sniffing to an external signal
  -- toplvl_status <= data_sniffing_status_internal;
